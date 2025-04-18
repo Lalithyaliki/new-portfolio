f52767fde38dbd5370d3ef6260d79a006e9025d7
Lalithyaliki <ananthaneniliki25@gmail.com>
2025-04-18 19:09:13 +0530

    updated code

--- script.js
+++ script.js
@@ -47,27 +47,76 @@ let popupmessage = document.getElementById("pop-msg")
 
 document.getElementById("pop").addEventListener("click", function () {
 
-    if (email.value.trim() != '') {
-        popupmessage.innerHTML =`your details are accessed successfully ${email.value}.`;
+    if ((email.value.trim() != '') && (email.value.includes("@gmail.com"))) {
+        popupmessage.innerHTML = `your details are accessed successfully ${email.value}.`;
+
 
         popupmessage.classList.add("success");
+
+
+        popupmessage.style.backgroundColor = 'blueviolet';
 
         setTimeout(() => {
             popupmessage.classList.remove('success');
         }, 3000);
 
         document.getElementById("chat").classList.remove("open");
+
+        email.value = '';
+        but.classList.remove('hide');
+
     }
     else {
-        popupmessage.innerHTML="enter valid email";
+        popupmessage.innerHTML = "enter valid email";
         popupmessage.classList.add("success");
-    
+
+        popupmessage.style.backgroundColor = 'brown';
+        popupmessage.style.textAlign = 'center';
+
         setTimeout(() => {
             popupmessage.classList.remove("success");
         }, 3000);
     }
 
 });
+
+var home = document.querySelector(".home .outer .inner");
+var profile1 =document.querySelector(".about .about1");
+var profile2 =document.querySelector(".about .about2");
+var skills=document.querySelectorAll(".equal .inner");
+
+var observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("slow");
+        }
+    });
+} , {
+    threshold : .9
+});
+
+observer.observe(home);
+observer.observe(profile1);
+observer.observe(profile2);
+skills.forEach(ele => {
+    observer.observe(ele);
+} );
+skills.forEach(ele => {
+    observer.observe(ele);
+} );
+
+
+
+
+  
+  
+
+
+
+
+
+
+
 
 
 
